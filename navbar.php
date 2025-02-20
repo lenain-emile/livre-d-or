e401fbba386c1d356e5aaf34a6749c0bfd848c09
florian-arnaud <florian.arnaud@laplateforme.io>
2025-02-20 23:04:55 +0100

    Added style for select user role in profile

--- navbar.php
+++ navbar.php
@@ -7,7 +7,7 @@ $user_id = $_SESSION['user_id'] ?? null;
 ?>
 
 <header class="header">
-    <nav class="nav">
+    <nav class="nav desktop">
         <ul>
             <li>
                 <div class="logo">Livre d'or</div>
@@ -17,7 +17,28 @@ $user_id = $_SESSION['user_id'] ?? null;
                 <li><a href="profile.php?id=<?= $user_id ?>">Profil</a></li>
                 <li><a href="guestbook.php">Livre d'or</a></li>
                 <li><a href="logout.php">Se déconnecter</a></li>
-                <?php if($_SESSION['user_permissions'])
+                <?php if($_SESSION['user_permissions'] == 2)
+                {?>
+                <li><a href="admin.php">Administration</a></li>
+                <?php } ?>
+            <?php } else { ?>
+                <li><a href="login.php">Se connecter</a></li>
+                <li><a href="register.php">S'inscrire</a></li>
+            <?php } ?>
+        </ul>
+    </nav>
+
+    <nav class="nav mobile">
+        <ul>
+            <li>
+                <div class="logo">Livre d'or</div>
+            </li>
+            <li><a href="index.php">Accueil</a></li>
+            <?php if ($user->isLoggedIn()) { ?>
+                <li><a href="profile.php?id=<?= $user_id ?>">Profil</a></li>
+                <li><a href="guestbook.php">Livre d'or</a></li>
+                <li><a href="logout.php">Se déconnecter</a></li>
+                <?php if($_SESSION['user_permissions'] == 2)
                 {?>
                 <li><a href="admin.php">Administration</a></li>
                 <?php } ?>
